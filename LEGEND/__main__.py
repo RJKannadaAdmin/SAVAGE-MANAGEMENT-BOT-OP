3758f2d0da5f5a35d3746494e5f434643800c383
RJKannadaAdmin <83166159+RJKannadaAdmin@users.noreply.github.com>
2021-04-28 14:26:15 +0530

    Update __main__.py

--- LEGEND/__main__.py
+++ LEGEND/__main__.py
@@ -106,7 +106,7 @@ HELP_STRINGS = f"""
 
 DONATE_STRING = """Heya, glad to hear you want to donate!
 You can donate to the original writer's of the Base code,
-Support them [The Hacker](t.me/The_Hacker_XD),"""
+Support them [The Hacker](t.me/TheHacker90),"""
 
 IMPORTED = {}
 MIGRATEABLE = []
